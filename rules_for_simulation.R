2d771b2ce3f81e616068b616f29bcce8ea309a9b
epiluis <luissegura.md@gmail.com>
2020-05-03 11:18:17 -0500

    Rules of the data generating process for simulation

---
+++ rules_for_simulation.R
@@ -0,0 +1,45 @@
+##################################################################
+#                                                                #
+#                     Rules for Simulation                       #
+#                                                                #
+# Rule 1: The prevalence of C is constant at each time t         #
+# Rule 2: C causes D and E                                       #
+# Rule 3: The prevalence of U2, & U4 are constant over time      #
+# Rule 4: We are assuming monotonicity for causes of E & D       #
+# Rule 5: P4 is constant for E at 0.60 and for D at 0.70         #
+# Rule 6: The prevalence of the totality of the causes of E      #
+#         is 1 - P4 for E. The prevalence of all causes of D     #
+#         is 1 - P4 for D.                                       #
+# Rule 6: The preevalence eof U1 decreases over time             #
+# Rule 7: The prevalence of U3 increases over time               #
+# Rule 8: Pr(E) = 1 if U3 or C and U4                            #
+# Rule 9: Pr(D) = 1 if C and U1 or U2. E does not cause D        #
+#                                                                #
+##################################################################
+
+
+library(tidyverse)
+
+### Exponential growth function
+exp_fun <- function(x, r, t){
+  x * ((1 + r) ^ t)
+}
+
+
+### Prevalence of Causal Partners
+
+u3 <- rep(NA, 10)
+
+for(i in 0:9){
+  u3[i + 1] <- exp_fun(0.04, 0.285, i)
+}
+
+rules <- tibble(time = 1:10, 
+                u1 = 0.25, 
+                u2 = 0.05, 
+                RR_D_C = (u1 + u2) / u2,
+                u3 = u3, 
+                u4 = 0.40 - u3, 
+                RR_E_C = (u3 + u4) / u3)
+
+rules
